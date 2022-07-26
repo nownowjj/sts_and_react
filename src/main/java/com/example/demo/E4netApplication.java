157d0ee3867bc5772eae8d5ff1aa523ec279883e
vsjiwon <sbddr3@gmail.com>
2022-07-26 15:55:12 +0900

    first

---
+++ src/main/java/com/example/demo/E4netApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4netApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E4netApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+	
+	@RequestMapping("/home")
+	public String hello(Model model,HttpServletRequest request) {
+
+		return "hello spring";
+	}
+	
+	
+
+}
